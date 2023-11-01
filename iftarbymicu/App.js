7139fe7a39d7d8600582a01942c1fa2270711ee3
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-11-01 23:07:21 +0000

    sucesss

--- iftarbymicu/App.js
+++ iftarbymicu/App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from "tamagui";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Button>Hello world</Button>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
