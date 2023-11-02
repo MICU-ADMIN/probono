e86cad32c0a14c5e2e7980d1fd6c10d883d3c501
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-11-02 13:15:41 +0000

    working

--- myapp/packages/app/features/home/screen.tsx
+++ myapp/packages/app/features/home/screen.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import {
   Anchor,
   Button,
@@ -10,13 +11,37 @@ import {
   YStack,
 } from '@my/ui'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
-import React, { useState } from 'react'
 import { useLink } from 'solito/link'
+import { Form, H4, Spinner } from 'tamagui'
+import { Toast, useToastState } from '@tamagui/toast'
+import { Label, Switch } from 'tamagui'
+
+
+
+
+
+
+
+
+
+
+
 
 export function HomeScreen() {
+  const [open, setOpen] = useState(false)
+  const [data, setData] = useState(3409283240832)
+
   const linkProps = useLink({
     href: '/user/nate',
   })
+
+  useEffect(() => {
+    data && setOpen(true)
+
+    return () => {
+      setData(null)
+    }
+  }, [data])
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
@@ -49,13 +74,12 @@ export function HomeScreen() {
         <Button {...linkProps}>Link to user</Button>
       </XStack>
 
-      <SheetDemo />
+      <SheetDemo data={data} open={open} setOpen={setOpen} />
     </YStack>
   )
 }
 
-function SheetDemo() {
-  const [open, setOpen] = useState(false)
+function SheetDemo({ open, data, setOpen }) {
   const [position, setPosition] = useState(0)
   const toast = useToastController()
 
@@ -72,7 +96,7 @@ function SheetDemo() {
         animation="medium"
         open={open}
         onOpenChange={setOpen}
-        snapPoints={[80]}
+        snapPoints={[30]}
         position={position}
         onPositionChange={setPosition}
         dismissOnSnapToBottom
@@ -80,19 +104,72 @@ function SheetDemo() {
         <Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
         <Sheet.Frame ai="center" jc="center">
           <Sheet.Handle />
-          <Button
-            size="$6"
-            circular
-            icon={ChevronDown}
-            onPress={() => {
-              setOpen(false)
-              toast.show('Sheet closed!', {
-                message: 'Just showing how toast works...',
-              })
-            }}
-          />
+          <FormValidator data={data} open={open} setOpen={setOpen} size={'$5'} />
         </Sheet.Frame>
       </Sheet>
     </>
   )
+}
+
+function FormValidator({ size, open, setOpen, data }) {
+  const [status, setStatus] = useState<'off' | 'submitting' | 'submitted'>('off')
+  const [response, setResponse] = useState(null)
+
+  const toast = useToastController()
+
+  useEffect(() => {
+    data && setStatus('submitting')
+    return () => {
+      setStatus('off')
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (status === 'submitting') {
+      checkValidity(data).then((result) => {
+        setResponse(result)
+        setTimeout(() => {
+          setStatus('off')
+          setOpen(false)
+          toast.show(`${result}`, {
+            message:
+              result === 'Invalid'
+                ? 'Nothing found, perhaps try again.'
+                : 'Sucessful read please continue',
+          })
+        }, 2000)
+      })
+    }
+  }, [status, data])
+
+  return (
+    <Form
+      alignItems="center"
+      minWidth={300}
+      gap="$2"
+      onSubmit={() => setStatus('submitting')}
+      // borderWidth={1}
+      borderRadius="$4"
+      backgroundColor="$background"
+      // borderColor="$borderColor"
+      // padding="$8"
+    >
+      <H4>{status[0].toUpperCase() + status.slice(1)}</H4>
+
+      <Form.Trigger asChild disabled={status !== 'off'}>
+        <Button icon={status === 'submitting' ? () => <Spinner /> : undefined}>Checking</Button>
+      </Form.Trigger>
+    </Form>
+  )
+}
+
+// UTIL
+
+const checkValidity = async (data) => {
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      const result = Math.random() < 0.5
+      resolve(result ? 'Valid' : 'Invalid')
+    }, 2000)
+  })
 }
